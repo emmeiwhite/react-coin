c1a48fbfdbe8e6de385259d7f050789651eaa0ea
emmeiwhite <emmeiwhite@gmail.com>
2020-09-30 16:38:37 +0530

    Created a working Detail Component and  using dynamic route and making another API call

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import logo from './../../logo.svg';
-import './Header.css'
+import React from "react";
+import logo from "./../../logo.svg";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 export default function Header() {
   return (
-      <header className="App-header">
-          <img src={logo} alt="react logo" height={80}/>
-          <h1>React Coin</h1>
-      </header>
-  )
+    <header className="App-header">
+      <img src={logo} alt="react logo" height={80} />
+      <Link
+        to="/"
+        style={{ display: "inline", textDecoration: "none", color: "#fff" }}
+      >
+        <h1>Cryptocoin</h1>
+      </Link>
+    </header>
+  );
 }
--- src/components/details/Detail.js
+++ src/components/details/Detail.js
@@ -1,12 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { withRouter } from "react-router-dom";
 import "./Detail.css";
 
 const Detail = ({ id, match, location, history }) => {
+  const [currencyData, setCurrencyData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    fetch(
+      `https://api.udilia.com/coins/v1/cryptocurrencies/${match.params.currencyId}`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Success", data);
+        setCurrencyData(data);
+      })
+      .catch((error) => {
+        console.log("Error", error);
+        setError(true);
+      });
+  }, []);
+
   return (
-    <div>
-      <h1>Detail Page</h1>
-      <p>Query Parameter being passed is:{match.params.currencyId}</p>
+    <div className="detail-wrapper">
+      {Object.keys(currencyData).length > 0 && (
+        <section className="detail-card">
+          <h1>Currencies Data</h1>
+          <p>
+            <strong>Name : </strong>
+            {currencyData.name}
+          </p>
+          <p>
+            <strong>Symbol: </strong>
+            {currencyData.symbol}
+          </p>
+          <p>
+            <strong>Rank:</strong> {currencyData.rank}
+          </p>
+          <p>
+            <strong>Volume24h: </strong>
+            {currencyData.volume24h}
+          </p>
+          <p>
+            <strong>MarketCap: </strong>
+            {currencyData.marketCap}
+          </p>
+          <p>
+            <strong>TotalSupply:</strong> {currencyData.totalSupply}
+          </p>
+          <p>
+            <strong>PercentChange24h:</strong> {currencyData.percentChange24h}
+          </p>
+
+          <h2 style={{ color: "#ff0000" }}>Styling is Pending ...</h2>
+        </section>
+      )}
     </div>
   );
 };
